8aa70c9f7f3d2b97440d6674595d68478eab567e
Soumya <soumyakatta21@gmail.com>
2024-03-01 12:01:43 +0530

    change the path of folderdata

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,6 @@
 import { createContext, useState } from "react";
-
 import "../css-files/Home.css";
-import FolderData from "./FolderData";
+import FolderData from "../StaticData/FolderData";
 import SideBar from "./SideBar";
 import DriveContent from "./DriveContent";
 
@@ -9,6 +8,7 @@ const folderItemsContext = createContext();
 const popUpContext = createContext();
 
 export default function Home() {
+   
   const folders = JSON.parse(localStorage.getItem("folderItems")) || FolderData;
   const [folderItems, setFolderItems] = useState(folders);
   const [isPopUpOpen, setIsPopUpOpen] = useState(null);
