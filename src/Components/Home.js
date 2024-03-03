c48e50cd59ceaa9c8a9df94749068602626745d2
Soumya <soumyakatta21@gmail.com>
2024-03-03 18:32:51 +0530

    import popupcontext provider into the component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,28 +3,31 @@ import "../css-files/Home.css";
 import FolderData from "../StaticData/FolderData";
 import SideBar from "./SideBar";
 import DriveContent from "./DriveContent";
-
-const folderItemsContext = createContext();
-const popUpContext = createContext();
+import { useRef } from "react";
+import PopUpContextProvider from "../Context/popUpContext";
+// const folderItemsContext = createContext();
+// const popUpContext = createContext();
 
 export default function Home() {
-  const folders = JSON.parse(localStorage.getItem("folderItems")) || FolderData;
-  const [folderItems, setFolderItems] = useState(folders);
-  const [isPopUpOpen, setIsPopUpOpen] = useState(null);
+  // const folders = JSON.parse(localStorage.getItem("folderItems")) || FolderData;
+  // const [folderItems, setFolderItems] = useState(folders);
+  //  const [isPopUpOpen, setIsPopUpOpen] = useState({action:null,foldf:null,name:null});
 
   return (
     <>
-      <folderItemsContext.Provider value={{ folderItems, setFolderItems }}>
-        <popUpContext.Provider value={{ isPopUpOpen, setIsPopUpOpen }}>
+      <PopUpContextProvider>
+      {/* <folderItemsContext.Provider value={{ folderItems, setFolderItems }}>
+        <popUpContext.Provider value={{ isPopUpOpen, setIsPopUpOpen }}> */}
           <div className="main-container">
             <SideBar />
             <div className="inner-container">
               <DriveContent />
             </div>
           </div>
-        </popUpContext.Provider>
-      </folderItemsContext.Provider>
+        {/* </popUpContext.Provider>
+      </folderItemsContext.Provider> */}
+      </PopUpContextProvider>
     </>
   );
 }
-export { folderItemsContext, popUpContext };
+// export { folderItemsContext, popUpContext };
