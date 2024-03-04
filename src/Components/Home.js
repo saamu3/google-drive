a24b3dc606b0c47671bf46c26fae396e917bba78
Soumya <soumyakatta21@gmail.com>
2024-03-04 18:00:28 +0530

    create folderitemscontext in the component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,25 @@
-import PopUpContextProvider from "../Context/popUpContext";
+import { createContext,useState } from "react";
+import FolderData from "../StaticData/FolderData";
 import "../css-files/Home.css";
 import DriveContent from "./DriveContent";
 import SideBar from "./SideBar";
+
+const FolderItemsContext = createContext()
 export default function Home() {
+  const folders = JSON.parse(localStorage.getItem("folderItems")) || FolderData;
+  const [folderItems, setFolderItems] = useState(folders);
   return (
     <>
-      <PopUpContextProvider>
+      <FolderItemsContext.Provider value={{folderItems, setFolderItems}}>
           <div className="main-container">
             <SideBar />
             <div className="inner-container">
               <DriveContent />
             </div>
           </div>
-      </PopUpContextProvider>
+      </FolderItemsContext.Provider>
     </>
   );
 }
+export {FolderItemsContext}
 
