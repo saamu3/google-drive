e29a51376d24fa8554793318576c4ac24f0ca925
Soumya <soumyakatta21@gmail.com>
2024-03-05 13:17:47 +0530

    create modelpopup ts file

---
+++ src/Components/ModelPopup.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import React from "react";
+import "../css-files/ModelPopup.css";
+
+interface  ModelPopProp {
+  handleButtonAction: void;
+  header: String;
+  buttonTitle: String;
+  folderId: number | null;
+  name: String | null;
+  handleClose: void;
+}
+export default function ModelPopup({
+  handleButtonAction,
+  header,
+  buttonTitle,
+  folderId = null,
+  name = null,
+  handleClose,
+}) {
+  const [folderName, setFolderName] = useState("Untitled folder");
+  return (
+    <div className="folder-card">
+      <h2>{header}</h2>
+      <input
+        className="folder-input"
+        type="text"
+        onChange={(event) => {
+          setFolderName(event.target.value);
+        }}
+        defaultValue={folderId ? name : folderName}
+      />
+      <div className="folder-buttons-container">
+        <button className="cancel-button" onClick={handleClose}>
+          Cancel
+        </button>
+
+        <button
+          className="create-button"
+          onClick={() => {
+            folderId
+              ? handleButtonAction(folderId, folderName)
+              : handleButtonAction(folderName);
+          }}
+        >
+          {buttonTitle}
+        </button>
+      </div>
+    </div>
+  );
+}
