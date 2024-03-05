ba1ba4441926a154a7d743bb9e3cd3d981c8c778
Soumya <soumyakatta21@gmail.com>
2024-03-05 13:14:19 +0530

    modify the folderData js to folderdata ts file

---
+++ src/StaticData/FolderData.tsx
@@ -0,0 +1,12 @@
+import { Items } from "../Components/Home";
+const FolderData:Items = [
+  {
+    id: 1,
+    name: "soumya",
+  },
+  {
+    id: 2,
+    name: "sakshi",
+  },
+];
+export default FolderData;
