da8e5e6ffff0871b6251d7c67ee0b43b1dcf3951
Soumya <soumyakatta21@gmail.com>
2024-03-11 11:53:51 +0530

    give folderid as string

--- src/StaticData/FilesData.tsx
+++ src/StaticData/FilesData.tsx
@@ -1,9 +1,10 @@
-const fileData = [
-    {file_id:1,folder_id:1,name:"file1.txt"},
-    {file_id:2,folder_id:1,name:"file2.txt"},
-    {file_id:3,folder_id:2,name:"file3.txt"},
-    {file_id:4,folder_id:2,name:"file4.txt"},
-    {file_id:5,folder_id:3,name:"file5.txt"},
-    {file_id:6,folder_id:3,name:"file6.txt"}
-]
+import { TFile } from "../Components/FolderContent";
+const fileData: TFile[] = [
+  { file_id: 1, folder_id: "1", name: "file1.txt" },
+  { file_id: 2, folder_id: "1", name: "file2.txt" },
+  { file_id: 3, folder_id: "2", name: "file3.txt" },
+  { file_id: 4, folder_id: "2", name: "file4.txt" },
+  { file_id: 5, folder_id: "3", name: "file5.txt" },
+  { file_id: 6, folder_id: "3", name: "file6.txt" },
+];
 export default fileData;
