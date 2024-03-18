0ffad72fd2d0a535e5de200a628b582baba958e8
Soumya <soumyakatta21@gmail.com>
2024-03-18 10:47:18 +0530

    update the home component

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,34 +1,17 @@
-import { createContext, useState } from "react";
-import FolderData from "../StaticData/FolderData";
 import "../css-files/Home.css";
 import DriveContent from "./DriveContent";
+import ContextProvider from "../context/FolderItems";
 import SideBar from "./SideBar";
-import React from "react";
-export type TFolderItems = {
-  id: string;
-  name: string;
-};
 
-export type TContext = {
-  folderItems: TFolderItems[];
-  setFolderItems: React.Dispatch<React.SetStateAction<TFolderItems[]>>;
-};
-export const FolderItemsContext = createContext<TContext>({
-  folderItems: FolderData,
-  setFolderItems: () => {},
-});
 export default function Home() {
-  const folders: TFolderItems[] =
-    JSON.parse(localStorage.getItem("folderItems") || "") || FolderData;
-  const [folderItems, setFolderItems] = useState<TFolderItems[]>(folders);
   return (
-    <FolderItemsContext.Provider value={{ folderItems, setFolderItems }}>
+    <ContextProvider>
       <div className="main-container">
         <SideBar />
         <div className="inner-container">
           <DriveContent />
         </div>
       </div>
-    </FolderItemsContext.Provider>
+    </ContextProvider>
   );
 }
