b0e88c15226e8935f7d2b0433f577232f7c46134
Soumya <soumyakatta21@gmail.com>
2024-03-18 10:59:11 +0530

    update the folderdata

--- src/StaticData/FolderData.tsx
+++ src/StaticData/FolderData.tsx
@@ -1,12 +1,11 @@
-import { TFolderItems } from "../Components/Home";
-import { v4 as uuidv4 } from "uuid";
-const FolderData:TFolderItems[] = [
+import { TFolderItems } from "../context/FolderItems";
+const FolderData: TFolderItems[] = [
   {
-    id: uuidv4(),
+    id: "1",
     name: "soumya",
   },
   {
-    id: uuidv4(),
+    id: "2",
     name: "sakshi",
   },
 ];
